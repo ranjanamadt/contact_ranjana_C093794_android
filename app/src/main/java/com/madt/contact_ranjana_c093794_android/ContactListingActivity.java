f133493263865a5e94cff1440d9495d1b6639bf5
ranjana <ranjana.madt@gmail.com>
2021-06-15 12:08:41 +0530

    Call, SMS, Email Functionality Added

--- app/src/main/java/com/madt/contact_ranjana_c093794_android/ContactListingActivity.java
+++ app/src/main/java/com/madt/contact_ranjana_c093794_android/ContactListingActivity.java
@@ -1,21 +1,24 @@
 package com.madt.contact_ranjana_c093794_android;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -23,13 +26,12 @@ import com.madt.contact_ranjana_c093794_android.adapter.RecyclerViewAdapter;
 import com.madt.contact_ranjana_c093794_android.model.Contact;
 import com.madt.contact_ranjana_c093794_android.model.ContactViewModel;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
 import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 
-public class ContactListingActivity extends AppCompatActivity implements RecyclerViewAdapter.OnContactClickListener {
+public class ContactListingActivity extends AppCompatActivity implements RecyclerViewAdapter.OnContactLongPressListener, RecyclerViewAdapter.OnEditContactClickListener {
     private ContactViewModel contactViewModel;
 
     private RecyclerView rcContacts;
@@ -37,6 +39,9 @@ public class ContactListingActivity extends AppCompatActivity implements Recycle
     public static final String CONTACT_ID = "contact_id";
     public TextView txtNoContactFound;
     private Contact deletedContact;
+    private List<Contact> contactList = new ArrayList<>();
+    public TextView txtContactsCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,11 +51,31 @@ public class ContactListingActivity extends AppCompatActivity implements Recycle
 
         rcContacts = findViewById(R.id.rcContacts);
         txtNoContactFound = findViewById(R.id.txtNoFound);
+        txtContactsCount = findViewById(R.id.txtContactCount);
+
+        // getting search view of our item.
+        SearchView searchView = findViewById(R.id.searchView);
+
+        // below line is to call set on query text listener method.
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // inside on query text change method we are
+                // calling a method to filter our recycler view.
+                filter(newText);
+                return false;
+            }
+        });
 
         rcContacts.setHasFixedSize(true);
         rcContacts.setLayoutManager(new LinearLayoutManager(this));
 
-     
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(v -> {
             Intent intent = new Intent(ContactListingActivity.this, AddContactActivity.class);
@@ -63,36 +88,64 @@ public class ContactListingActivity extends AppCompatActivity implements Recycle
         itemTouchHelper.attachToRecyclerView(rcContacts);
     }
 
+    private void filter(String text) {
+        // creating a new array list to filter our data.
+        ArrayList<Contact> filteredlist = new ArrayList<>();
+
+        // running a for loop to compare elements.
+        for (Contact item : contactList) {
+            // checking if the entered string matched with any item of our recycler view.
+            if (item.getFirstName().toLowerCase().contains(text.toLowerCase())||item.getLastName().toLowerCase().contains(text.toLowerCase())) {
+                // if the item is matched we are
+                // adding it to our filtered list.
+                filteredlist.add(item);
+            }
+        }
+        if (filteredlist.isEmpty()) {
+            // if no item is added in filtered list we are
+            // displaying a toast message as no data found.
+            txtNoContactFound.setVisibility(View.VISIBLE);
+        } else {
+            // at last we are passing that filtered
+            // list to our adapter class.
+            txtNoContactFound.setVisibility(View.INVISIBLE);
+
+        }
+        setContactsCount(filteredlist.size());
+        rcContactsAdapter.filterList(filteredlist);
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
+        Log.e("in","resumer");
         contactViewModel.getAllContacts().observe(this, contacts -> {
-
-            if(contacts.isEmpty()){
+            contactList.clear();
+            contactList.addAll(contacts);
+            if (contacts.isEmpty()) {
                 txtNoContactFound.setVisibility(View.VISIBLE);
-            }else {
+                rcContacts.setVisibility(View.INVISIBLE);
+                setContactsCount(contacts.size());
+            } else {
                 txtNoContactFound.setVisibility(View.INVISIBLE);
-                rcContactsAdapter = new RecyclerViewAdapter(contacts, this, this);
+                rcContacts.setVisibility(View.VISIBLE);
+                setContactsCount(contacts.size());
+                rcContactsAdapter = new RecyclerViewAdapter(contacts, this, this,this);
                 rcContacts.setAdapter(rcContactsAdapter);
             }
         });
     }
 
-    @Override
-    public void onContactClick(int position) {
-        Contact contact = contactViewModel.getAllContacts().getValue().get(position);
-        Intent intent = new Intent(ContactListingActivity.this, AddContactActivity.class);
-        intent.putExtra(CONTACT_ID, contact.getId());
-        startActivity(intent);
+    private void setContactsCount(int size) {
+        txtContactsCount.setText("(" + size + ")");
     }
 
-    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT ) {
 
         @Override
         public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
             return false;
         }
-
         @Override
         public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
             int position = viewHolder.getAdapterPosition();
@@ -100,21 +153,17 @@ public class ContactListingActivity extends AppCompatActivity implements Recycle
             switch (direction) {
                 case ItemTouchHelper.LEFT:
                     AlertDialog.Builder builder = new AlertDialog.Builder(ContactListingActivity.this);
-                    builder.setTitle("Are you sure?");
+                    builder.setTitle("Are you sure you want to delete this contact?");
                     builder.setPositiveButton("Yes", (dialog, which) -> {
                         deletedContact = contact;
                         contactViewModel.delete(contact);
-                        Snackbar.make(rcContacts, deletedContact.getFirstName() + " is deleted!", Snackbar.LENGTH_LONG)
+
+                        Snackbar.make(rcContacts, deletedContact.getFirstName() + deletedContact.getLastName()+ " is deleted!", Snackbar.LENGTH_LONG)
                                 .setAction("Undo", v -> contactViewModel.insert(deletedContact)).show();
                     });
                     builder.setNegativeButton("No", (dialog, which) -> rcContactsAdapter.notifyDataSetChanged());
                     AlertDialog alertDialog = builder.create();
                     alertDialog.show();
-                    break;
-                case ItemTouchHelper.RIGHT:
-                    Intent intent = new Intent(ContactListingActivity.this, AddContactActivity.class);
-                    intent.putExtra(CONTACT_ID, contact.getId());
-                    startActivity(intent);
                     break;
             }
         }
@@ -124,7 +173,6 @@ public class ContactListingActivity extends AppCompatActivity implements Recycle
             new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
                     .setIconHorizontalMargin(1, 1)
                     .addSwipeLeftActionIcon(R.drawable.ic_delete)
-                    .addSwipeRightActionIcon(R.drawable.ic_baseline_update_24)
                     .create()
                     .decorate();
             super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
@@ -132,4 +180,68 @@ public class ContactListingActivity extends AppCompatActivity implements Recycle
     };
 
 
+    @Override
+    public void onContactLongPress(int position) {
+        String MAKE_CALL = "Make a Call";
+        String SEND_SMS = "Send SMS";
+        String SEND_EMAIL = "Send email";
+
+        final CharSequence[] items = {MAKE_CALL, SEND_SMS, SEND_EMAIL};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+
+                switch (item) {
+                    case 0:
+                        makeACall(contactList.get(position).getPhoneNumber());
+                        break;
+                    case 1:
+                        sendSMS(contactList.get(position).getPhoneNumber());
+                        break;
+                    case 2:
+                        sendEmail(contactList.get(position).getEmail());
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + items[item].toString());
+                }
+
+            }
+        });
+        builder.show();
+
+
+    }
+
+    private void sendEmail(String toEmail) {
+        Intent mailIntent = new Intent(Intent.ACTION_SEND);
+        mailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{ toEmail});
+        mailIntent.putExtra(Intent.EXTRA_SUBJECT, "");
+        mailIntent.putExtra(Intent.EXTRA_TEXT, "");
+
+
+        mailIntent.setType("message/rfc822");
+        startActivity(Intent.createChooser(mailIntent, "Choose an Email client :"));
+    }
+
+    private void sendSMS(long phoneNumber) {
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + phoneNumber));
+        smsIntent.putExtra("sms_body", "");
+        startActivity(smsIntent);
+    }
+
+    private void makeACall(long phoneNumber) {
+            String dial = "tel:" + phoneNumber;
+            startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(dial)));
+    }
+
+    @Override
+    public void onEditContactClick(int position) {
+        Contact contact = contactViewModel.getAllContacts().getValue().get(position);
+        Intent intent = new Intent(ContactListingActivity.this, AddContactActivity.class);
+        intent.putExtra(CONTACT_ID, contact.getId());
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/madt/contact_ranjana_c093794_android/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/madt/contact_ranjana_c093794_android/adapter/RecyclerViewAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,18 +15,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.madt.contact_ranjana_c093794_android.R;
 import com.madt.contact_ranjana_c093794_android.model.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private List<Contact> contactList;
     private Context context;
-    private OnContactClickListener onContactClickListener;
+    private OnContactLongPressListener onContactLongPressListener;
+    private OnEditContactClickListener onEditContactClickListener;
 
-    public RecyclerViewAdapter(List<Contact> contactList, Context context, OnContactClickListener onContactClickListener) {
+    public RecyclerViewAdapter(List<Contact> contactList, Context context,OnContactLongPressListener onContactLongPressListener, OnEditContactClickListener onEditContactClickListener) {
         this.contactList = contactList;
         this.context = context;
-        this.onContactClickListener = onContactClickListener;
+        this.onContactLongPressListener = onContactLongPressListener;
+        this.onEditContactClickListener = onEditContactClickListener;
     }
 
     @NonNull
@@ -35,12 +39,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 .inflate(R.layout.list_row_contact, parent, false);
         return new ViewHolder(view);
     }
-
+    // method for filtering our recyclerview items.
+    public void filterList(ArrayList<Contact> filterllist) {
+        // below line is to add our filtered
+        // list in our course array list.
+        contactList = filterllist;
+        // below line is to notify our adapter
+        // as change in recycler view data.
+        notifyDataSetChanged();
+    }
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Contact contact = contactList.get(position);
         holder.txtFirstName.setText(contact.getFirstName()+" "+contact.getLastName());
-        holder.txtPhoneNumber.setText(contact.getPhoneNumber()+" ");
+        holder.txtPhoneNumber.setText(contact.getPhoneNumber()+"");
+
+        holder.txtTag.setText(contact.getFirstName().charAt(0)+""+contact.getLastName().charAt(0));
 
     }
 
@@ -49,39 +63,35 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return contactList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private TextView txtFirstName, txtPhoneNumber;
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnLongClickListener {
+        private TextView txtFirstName, txtPhoneNumber,txtTag;
+        private ImageView imgEdit;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtFirstName=itemView.findViewById(R.id.txt_name);
             txtPhoneNumber=itemView.findViewById(R.id.txt_phone_number);
+            txtTag=itemView.findViewById(R.id.txtTag);
+            imgEdit=itemView.findViewById(R.id.img_edit);
 
+            itemView.setOnLongClickListener(this);
+            imgEdit.setOnClickListener(v -> {
+                onEditContactClickListener.onEditContactClick(getAdapterPosition());
+            });
         }
-
-        /* private TextView name;
-                private TextView department;
-                private TextView hireDate;
-
-                public ViewHolder(@NonNull View itemView) {
-                    super(itemView);
-
-                    name = itemView.findViewById(R.id.name_row);
-                    department = itemView.findViewById(R.id.dept_row);
-                    hireDate = itemView.findViewById(R.id.hire_date_row);
-
-                    itemView.setOnClickListener(this);
-
-                }
-        */
+    
         @Override
-        public void onClick(View v) {
-            onContactClickListener.onContactClick(getAdapterPosition());
+        public boolean onLongClick(View v) {
+            onContactLongPressListener.onContactLongPress(getAdapterPosition());
+            return true;
         }
     }
 
-    public interface OnContactClickListener {
-        void onContactClick(int position);
+    public interface OnContactLongPressListener {
+        void onContactLongPress(int position);
+    }
+    public interface OnEditContactClickListener {
+        void onEditContactClick(int position);
     }
 }
 
